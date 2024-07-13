3261efb53b2a9b763d37eb5a34f1d490782d88eb
luq-mne <luqman.yazrie@outlook.com>
2024-07-14 01:50:02 +0800

    database

---
+++ scripts/database.go
@@ -0,0 +1,14 @@
+package scripts
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// DatabaseConnect opens a connection to a sqlite3 database.
+func DatabaseConnect(location string) (db *sql.DB, err error) {
+	db, err = sql.Open("sqlite3", location)
+	if err!= nil {
+		panic(err)
+	}
+}
