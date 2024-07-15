ca1020ed2748145731b3ee6f7c97f3f3bcf22c3c
luq-mne <luqman.yazrie@outlook.com>
2024-07-15 16:48:51 +0800

    select folder

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"strings"
 	"fmt"
 	"ascend/scripts"
@@ -8,13 +9,38 @@ import (
     _ "github.com/mattn/go-sqlite3"
 )
 
-
+var save_path string
 func main() {
+	ardent.ConsoleClear()
 	fmt.Println("ASCEND")
 
-	// Get the path to the save folder
-	fmt.Printf("Enter the path to the save folder: ")
-	save_path := strings.TrimSpace(ardent.ConsoleRead())
+	fmt.Println("[1] Connect to a save folder\n[2] Create a new one")
+	fmt.Printf("Select an option [1/2]: ")
+	option := strings.TrimSpace(ardent.ConsoleRead())
+	switch option {
+	case "1":
+		// Get the path to the save folder
+		fmt.Printf("Path to the save folder: ")
+		save_path = strings.TrimSpace(ardent.ConsoleRead())
+
+	case "2":
+		// Create the folder
+		fmt.Printf("Folder name: ")
+		folder_name := strings.TrimSpace(ardent.ConsoleRead())
+		fmt.Printf("Location: ")
+		folder_location := strings.TrimSpace(ardent.ConsoleRead())
+
+		err := os.MkdirAll(filepath.Join(folder_location, folder_name), 0755)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to create folder: %v", err))
+		}
+		fmt.Println("Folder created successfully.")
+
+		save_path = filepath.Join(folder_location, folder_name)
+
+	default:
+		panic("Invalid option. Please select either 1 or 2.")
+	}
 	
 	db:= ardent.DatabaseConnect(filepath.Join(save_path, "test.db"))
 
--- scripts/console.go
+++ scripts/console.go
@@ -3,10 +3,25 @@ package ardent
 import (
 	"bufio"
 	"os"
+	"runtime"
+	"os/exec"
 )
 
+// Get input from console
 func ConsoleRead() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 	return text
+}
+
+// Clear console
+func ConsoleClear() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
